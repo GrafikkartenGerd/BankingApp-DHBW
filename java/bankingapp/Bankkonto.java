3b2be127bb0b2ce40a6ebc42d6d446bde9a5d055
MrSebas3 <120368945+MrSebas3@users.noreply.github.com>
2023-06-23 17:05:50 +0200

    added Bankkonto

---
+++ java/bankingapp/Bankkonto.java
@@ -0,0 +1,25 @@
+package BankingApp;
+
+abstract class Bankkonto {
+
+    private String Kontonummer;
+    private double Kontostand;
+
+    public Bankkonto(String kontonummer, double kontostand){
+        this.Kontonummer = kontonummer;
+        this.Kontostand = kontostand;
+    }
+
+    public String getKontonummer(){
+        return Kontonummer;
+    }
+    public double getKontostand(){
+        return Kontostand;
+    }
+    public void einzahlen(double betrag){
+        Kontostand += betrag;
+    }
+    public void auszahlen(double betrag){
+        Kontostand -= betrag;
+    }
+}
