5eaa9c504e318d204a3103822c35e3b844c88501
TobiasRuhl <119286774+TobiasRuhl@users.noreply.github.com>
2023-06-23 18:22:25 +0200

    Update BankingAppUI.java

--- com/schneidergroup/bankingapp/BankingAppUI.java
+++ com/schneidergroup/bankingapp/BankingAppUI.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class BankingAppUI extends JFrame {
     private JTextField nameField;
     private JTextField vornameField;
@@ -15,27 +14,42 @@ public class BankingAppUI extends JFrame {
     public BankingAppUI() {
         setTitle("Banking App");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
+        setLayout(new GridLayout(6, 2, 10, 10)); // 6 Zeilen, 2 Spalten, Abstand von 10 Pixeln
 
         // Name
         JLabel nameLabel = new JLabel("Name:");
         nameField = new JTextField(20);
+        JPanel namePanel = new JPanel();
+        namePanel.add(nameLabel);
+        namePanel.add(nameField);
 
         // Vorname
         JLabel vornameLabel = new JLabel("Vorname:");
         vornameField = new JTextField(20);
+        JPanel vornamePanel = new JPanel();
+        vornamePanel.add(vornameLabel);
+        vornamePanel.add(vornameField);
 
         // Email
         JLabel emailLabel = new JLabel("Email:");
         emailField = new JTextField(20);
+        JPanel emailPanel = new JPanel();
+        emailPanel.add(emailLabel);
+        emailPanel.add(emailField);
 
         // Telefonnummer
         JLabel telefonnummerLabel = new JLabel("Telefonnummer:");
         telefonnummerField = new JTextField(20);
+        JPanel telefonnummerPanel = new JPanel();
+        telefonnummerPanel.add(telefonnummerLabel);
+        telefonnummerPanel.add(telefonnummerField);
 
         // Adresse
         JLabel adresseLabel = new JLabel("Adresse:");
         adresseField = new JTextField(20);
+        JPanel adressePanel = new JPanel();
+        adressePanel.add(adresseLabel);
+        adressePanel.add(adresseField);
 
         // Button
         JButton createButton = new JButton("Kunden erstellen");
@@ -58,20 +72,16 @@ public class BankingAppUI extends JFrame {
         });
 
         // UI-Komponenten zur UI hinzufügen
-        add(nameLabel);
-        add(nameField);
-        add(vornameLabel);
-        add(vornameField);
-        add(emailLabel);
-        add(emailField);
-        add(telefonnummerLabel);
-        add(telefonnummerField);
-        add(adresseLabel);
-        add(adresseField);
+        add(namePanel);
+        add(vornamePanel);
+        add(emailPanel);
+        add(telefonnummerPanel);
+        add(adressePanel);
         add(createButton);
 
         pack();
         setLocationRelativeTo(null); // Zentrieren der UI auf dem Bildschirm
         setVisible(true);
     }
+
 }
